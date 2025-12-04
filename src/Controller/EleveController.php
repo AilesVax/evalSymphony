983157183aad3363fb008a8c2584e1f30ea12efc
carls-eth <charles.bauchet@edu.devinci.fr>
2025-12-04 19:09:44 +0100

    Ajout du formulaire d'ajout d'élève

--- src/Controller/EleveController.php
+++ src/Controller/EleveController.php
@@ -2,9 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Eleve;
+use App\Entity\Classe;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 final class EleveController extends AbstractController
 {
@@ -13,6 +20,39 @@ final class EleveController extends AbstractController
     {
         return $this->render('eleve/index.html.twig', [
             'controller_name' => 'EleveController',
+        ]);
+    }
+
+    #[Route('/eleve/ajouter', name: 'app_eleve_ajouter')]
+    public function ajouter(Request $request, EntityManagerInterface $em): Response
+    {
+        $eleve = new Eleve();
+
+        $form = $this->createFormBuilder($eleve)
+            ->add('prenom', TextType::class, ['label' => 'Prénom'])
+            ->add('nom', TextType::class, ['label' => 'Nom'])
+            ->add('class', EntityType::class, [
+                'class' => Classe::class,
+                'choice_label' => 'nom',
+                'label' => 'Classe',
+                'placeholder' => 'choisir une classe',
+            ])
+            ->add('ajouter', SubmitType::class, ['label' => 'ajouter'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($eleve);
+            $em->flush();
+
+            $this->addFlash('success', 'élève ajouté');
+
+            return $this->redirectToRoute('app_eleve_ajouter');
+        }
+
+        return $this->render('eleve/ajouter.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
