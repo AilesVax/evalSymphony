7b1b86aecd5521ef5e2a674e98fff677a8c13f9e
AilesVax <raphael.vaxelaire10@gmail.com>
2025-12-04 17:17:06 +0100

    base de donnees

---
+++ src/Entity/Classe.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClasseRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClasseRepository::class)]
+class Classe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom_classe = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $niveau = null;
+
+    /**
+     * @var Collection<int, Eleve>
+     */
+    #[ORM\OneToMany(targetEntity: Eleve::class, mappedBy: 'class')]
+    private Collection $eleves;
+
+    public function __construct()
+    {
+        $this->eleves = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomClasse(): ?string
+    {
+        return $this->nom_classe;
+    }
+
+    public function setNomClasse(string $nom_classe): static
+    {
+        $this->nom_classe = $nom_classe;
+
+        return $this;
+    }
+
+    public function getNiveau(): ?string
+    {
+        return $this->niveau;
+    }
+
+    public function setNiveau(string $niveau): static
+    {
+        $this->niveau = $niveau;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Eleve>
+     */
+    public function getEleves(): Collection
+    {
+        return $this->eleves;
+    }
+
+    public function addElefe(Eleve $elefe): static
+    {
+        if (!$this->eleves->contains($elefe)) {
+            $this->eleves->add($elefe);
+            $elefe->setClass($this);
+        }
+
+        return $this;
+    }
+
+    public function removeElefe(Eleve $elefe): static
+    {
+        if ($this->eleves->removeElement($elefe)) {
+            // set the owning side to null (unless already changed)
+            if ($elefe->getClass() === $this) {
+                $elefe->setClass(null);
+            }
+        }
+
+        return $this;
+    }
+}
