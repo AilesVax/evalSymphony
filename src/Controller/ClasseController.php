6cc5a18f1dd5232779111f146b24e58497788934
max <maximilien.ilic@gmail.com>
2025-12-04 19:13:31 +0100

    controller + twig

--- src/Controller/ClasseController.php
+++ src/Controller/ClasseController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Classe;
+use App\Repository\ClasseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ClasseController extends AbstractController
 {
-    #[Route('/classe', name: 'app_classe')]
-    public function index(): Response
+    #[Route('/classe', name: 'app_classe', methods: ['GET'])]
+    public function index(ClasseRepository $classeRepository): Response
     {
+        $classes = $classeRepository->findAll();
+
         return $this->render('classe/index.html.twig', [
-            'controller_name' => 'ClasseController',
+            'classes' => $classes
         ]);
     }
 }
--- src/Controller/EleveController.php
+++ src/Controller/EleveController.php
@@ -5,14 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\EleveRepository;
+
 
 final class EleveController extends AbstractController
 {
-    #[Route('/eleve', name: 'app_eleve')]
-    public function index(): Response
+    #[Route('/eleve', name: 'app_eleve', methods: ['GET'])]
+    public function index(EleveRepository $eleveRepository): Response
     {
+        $eleves = $eleveRepository->findAll();
         return $this->render('eleve/index.html.twig', [
             'controller_name' => 'EleveController',
+            'eleves' => $eleves
+
         ]);
     }
+
 }
